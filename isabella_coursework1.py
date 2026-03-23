d96ddeca7567f5c2e002e7a7f33c63534e5264db
bella-marcucci <bellamarcucci@gmail.com>
2026-03-23 14:32:27 -0700

    Menu loop

--- isabella_coursework1.py
+++ isabella_coursework1.py
@@ -1,21 +1,59 @@
 inventory = {
     101: {"name": "Laptop",   "quantity": 5,  "price": 799.99, "category": "Electronics", "brand": "Dell"},
-    102: {"name": "Mouse", "quantity": 20, "price": 35.99,   "category": "Office",      "brand": "Logitech"},
-    103: {"name": "Bed Frame",    "quantity": 10, "price": 229.99,   "category": "Home",        "brand": "Ikea"},
 }
  
 categories = ["Electronics", "Home", "Office", "Food", "Clothing"]
  
-product_ids = {101, 102, 103}
+product_ids = {101}
  
-next_id = 104
+next_id = 102
  
 FILE_NAME = "inventory.txt"
  
-print("=== Sample Inventory Data ===")
-for pid, item in inventory.items():
-    print(f"ID: {pid} | Name: {item['name']} | Qty: {item['quantity']} | Price: ${item['price']:.2f} | Category: {item['category']}")
+def show_menu():
+    print("\n" + "=" * 43)
+    print("1. Add Item")
+    print("2. View Inventory")
+    print("3. Update Item")
+    print("4. Remove Item")
+    print("5. Exit")
+
+def add_item():
+    print("Not set yet.")
  
-print(f"\nCategories available: {categories}")
-print(f"Registered IDs (set): {product_ids}")
-print(f"Next ID to assign: {next_id}")
+def view_inventory():
+    print("Not set yet.")
+ 
+def update_item():
+    print("Not set yet.")
+ 
+def remove_item():
+    print("Not set yet.")
+ 
+def main():
+    print("Welcome to my Inventory Management System!")
+    print("=" * 43)
+ 
+   
+    while True:
+        show_menu()
+        print("Select an option: ", end="")
+        choice = input("> ").strip()
+ 
+        if choice == "1":
+            add_item()
+        elif choice == "2":
+            view_inventory()
+        elif choice == "3":
+            update_item()
+        elif choice == "4":
+            remove_item()
+        elif choice == "5":
+            print("Exiting system. Bye!")
+            break
+        else:
+            print("  [!]ERROR[!] Invalid option. Please select 1-5.")
+ 
+ 
+if __name__ == "__main__":
+    main()
